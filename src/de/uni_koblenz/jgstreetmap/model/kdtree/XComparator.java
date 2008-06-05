dd9cf64030f8be47fca672b8cff32a6a245a347f
Elisa Ziegler <eziegler@uni-koblenz.de>
2008-06-05 14:10:21 +0000

    implementation for the construction of a kdtree and for its query algorithm
    git-svn-id: https://svn.uni-koblenz.de/gupro/re-group/trunk/project/jgstreetmap@6241 6c03965d-96ef-0310-af27-ce2fa9dd3b5a

---
+++ src/de/uni_koblenz/jgstreetmap/model/kdtree/XComparator.java
@@ -0,0 +1,16 @@
+package de.uni_koblenz.jgstreetmap.model.kdtree;
+
+import java.util.Comparator;
+import de.uni_koblenz.jgstreetmap.osmschema.Node;
+
+public class XComparator implements Comparator<Node>{
+
+	public int compare(Node n1, Node n2) {
+		if(n1.getLongitude()<n2.getLongitude())
+			return -1;
+		if(n1.getLongitude()==n2.getLongitude())
+			return 0;
+		return 1;
+	}
+
+}
