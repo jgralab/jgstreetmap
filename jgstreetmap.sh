a70484d5e9cceb8064244c3495f9ecedcaae4dc9
Sascha Strauß <strauss@uni-koblenz.de>
2010-08-31 07:20:49 +0000

    Made "jgstreetmap.sh" run on (hopefully) every computer, not only Volker's. Added "standard" to implementation mode because otherwise the import fails.
    git-svn-id: https://svn.uni-koblenz.de/gupro/re-group/trunk/project/jgstreetmap@12371 6c03965d-96ef-0310-af27-ce2fa9dd3b5a

--- jgstreetmap.sh
+++ jgstreetmap.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-cd /Users/riediger/src/ist/local/jgstreetmap
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
-/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java -Xmx1000M -cp build/jar/jgstreetmap.jar:../jgralab/build/jar/jgralab.jar de.uni_koblenz.jgstreetmap.JGStreetMap $*
+java -Xmx1000M -cp build/jar/jgstreetmap.jar:../jgralab/build/jar/jgralab.jar de.uni_koblenz.jgstreetmap.JGStreetMap $*
